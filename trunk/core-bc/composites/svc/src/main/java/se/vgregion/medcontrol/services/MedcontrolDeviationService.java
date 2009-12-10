ebafd85181d23f2a6f753905b955400d1235cda3
asse.hemma <asse.hemma@e58ce578-e49c-11de-b136-e12340cf38ae>
2009-12-10 16:20:17 +0000

    Comments/doc added

--- trunk/core-bc/composites/svc/src/main/java/se/vgregion/medcontrol/services/MedcontrolDeviationService.java
+++ trunk/core-bc/composites/svc/src/main/java/se/vgregion/medcontrol/services/MedcontrolDeviationService.java
@@ -42,68 +42,81 @@ import se.vgregion.portal.medcontrol.ws.MyCasesServiceSoap;
  */
 public class MedcontrolDeviationService implements DeviationService {
 
-  private static final Logger LOGGER = LoggerFactory.getLogger(MedcontrolDeviationService.class);
-  private MyCasesServiceSoap myCasesServiceSoap;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MedcontrolDeviationService.class);
+    private MyCasesServiceSoap myCasesServiceSoap;
 
-  @Autowired
-  public void setMyCasesServiceSoap(MyCasesServiceSoap myCasesServiceSoap) {
-    this.myCasesServiceSoap = myCasesServiceSoap;
-  }
-
-  /**
-   * 
-   * {@inheritDoc}
-   */
-  @Override
-  public List<DeviationCase> listDeviationCases(String userId) {
-    return populateDeviations(userId);
-  }
-
-  private List<DeviationCase> populateDeviations(String userId) {
-    List<DeviationCase> deviationCases = new ArrayList<DeviationCase>();
-    if (!StringUtils.isBlank(userId)) {
-      ArrayOfCase userCases = callService(userId);
-      List<Case> cases = userCases.getCase();
-      DeviationCase deviationCase = null;
-      for (Case medcontrolCase : cases) {
-        deviationCase = new DeviationCase();
-        deviationCase.setCaseNumber(medcontrolCase.getCaseNo());
-        deviationCase.setDescription(medcontrolCase.getDescription());
-        deviationCase.setHasActingRole(medcontrolCase.isHasActingRole());
-        deviationCase.setRegisteredDate(parseDate(medcontrolCase.getRegisteredDate()));
-        deviationCase.setUrl(medcontrolCase.getUrl());
-        deviationCases.add(deviationCase);
-      }
-    }
-    return deviationCases;
-  }
-
-  private ArrayOfCase callService(String userId) {
-    ArrayOfCase arrayOfCase = new ArrayOfCase();
-
-    try {
-      arrayOfCase = myCasesServiceSoap.getUserCases(userId, true, true, "");
-    } catch (WebServiceException e) {
-      LOGGER.error("MedControl webservice exception", e);
-      throw new DeviationServiceException("Unhandled exception from webservice", e);
+    @Autowired
+    public void setMyCasesServiceSoap(MyCasesServiceSoap myCasesServiceSoap) {
+        this.myCasesServiceSoap = myCasesServiceSoap;
     }
 
-    return arrayOfCase;
-  }
-
-  private Date parseDate(String date) {
-
-    Date caseDate = null;
-    if (date != null) {
-      SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
-      simpleDateFormat.setLenient(false);
-      try {
-        caseDate = simpleDateFormat.parse(date);
-      } catch (ParseException e) {
-        LOGGER.error("Not valid date", e);
-      }
+    /**
+     * 
+     * {@inheritDoc}
+     */
+    @Override
+    public List<DeviationCase> listDeviationCases(String userId) {
+        return populateDeviations(userId);
     }
-    return caseDate;
-  }
+
+    private List<DeviationCase> populateDeviations(String userId) {
+        List<DeviationCase> deviationCases = new ArrayList<DeviationCase>();
+        if (!StringUtils.isBlank(userId)) {
+            ArrayOfCase userCases = callService(userId);
+            List<Case> cases = userCases.getCase();
+            DeviationCase deviationCase = null;
+            for (Case medcontrolCase : cases) {
+                deviationCase = new DeviationCase();
+                deviationCase.setCaseNumber(medcontrolCase.getCaseNo());
+                deviationCase.setDescription(medcontrolCase.getDescription());
+                deviationCase.setHasActingRole(medcontrolCase.isHasActingRole());
+                deviationCase.setRegisteredDate(parseDate(medcontrolCase.getRegisteredDate()));
+                deviationCase.setUrl(medcontrolCase.getUrl());
+                deviationCases.add(deviationCase);
+            }
+        }
+        return deviationCases;
+    }
+
+    private ArrayOfCase callService(String userId) {
+        ArrayOfCase arrayOfCase = new ArrayOfCase();
+
+        // userId = [lämpligt VGR-id]
+        // checkForActingRole = true (anger om man vill kontrollera om man har en "aktiv" roll, bra, men lite
+        // "dyrt") includeOnlyActingRole = false (anger att man bara vill ha ärenden där man har en aktiv roll)
+        // culture = sv-SE
+        //
+        // För att förtydliga de två booleska parametrarna:
+        // False, false -> Hämtar alla mina ärenden (motsvarar pågående-fliken, värdet för HasActingRole är inte
+        // satt och säger inget).
+        // False, true -> Hämtar bara ärenden där jag har en aktiv roll (motsvarar todo-fliken).
+        // True, false -> Hämtar alla mina ärenden och anger i vilka jag har en aktiv roll (motsvarar pågående- och
+        // todo-fliken, värdet för HasActingRole anger vilka ärenden som även visas som "todo").
+        // True, true - Meningslös kombination, funkar säkert men svaret är lite odefinierat.
+
+        try {
+            arrayOfCase = myCasesServiceSoap.getUserCases(userId, false, true, "sv-SE");
+        } catch (WebServiceException e) {
+            LOGGER.error("MedControl webservice exception", e);
+            throw new DeviationServiceException("Unhandled exception from webservice", e);
+        }
+
+        return arrayOfCase;
+    }
+
+    private Date parseDate(String date) {
+
+        Date caseDate = null;
+        if (date != null) {
+            SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+            simpleDateFormat.setLenient(false);
+            try {
+                caseDate = simpleDateFormat.parse(date);
+            } catch (ParseException e) {
+                LOGGER.error("Not valid date", e);
+            }
+        }
+        return caseDate;
+    }
 
 }
