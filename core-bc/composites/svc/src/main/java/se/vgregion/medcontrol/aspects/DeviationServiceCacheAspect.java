6b92d05f073be3f850c455a3452e6aaa51f02415
asse.hemma <asse.hemma@e58ce578-e49c-11de-b136-e12340cf38ae>
2009-12-14 09:12:48 +0000

    First attempt to add ehcache, not completed

---
+++ core-bc/composites/svc/src/main/java/se/vgregion/medcontrol/aspects/DeviationServiceCacheAspect.java
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2009 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ */
+package se.vgregion.medcontrol.aspects;
+
+import javax.annotation.Resource;
+
+import net.sf.ehcache.Cache;
+import net.sf.ehcache.Element;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.annotation.Order;
+import org.springframework.stereotype.Component;
+
+@Component("deviationServiceCacheAspect")
+@Aspect
+@Order(2)
+public class DeviationServiceCacheAspect {
+    @Resource(name = "deviationCasesCache")
+    private Cache cache;
+
+    @Around("execution(* se.vgregion.medcontrol.services.MedcontrolDeviationService.listDeviationCases(java.lang.String))")
+    public Object cacheDeviationCases(ProceedingJoinPoint joinPoint) throws Throwable {
+        Object[] arguments = joinPoint.getArgs();
+        String location = (String) arguments[0];
+        Element element = cache.get(location);
+        if (null == element) {
+            Object result = joinPoint.proceed();
+            if (null != result) {
+                element = new Element(location, result);
+                cache.put(element);
+            }
+        }
+        return element != null ? element.getValue() : null;
+    }
+}
