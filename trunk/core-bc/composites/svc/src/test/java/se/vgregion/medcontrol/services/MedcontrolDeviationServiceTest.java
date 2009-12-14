5c3b17f6347bb67eeb2775d7a20ce66f22a55a1f
asse.hemma <asse.hemma@e58ce578-e49c-11de-b136-e12340cf38ae>
2009-12-14 09:12:48 +0000

    First attempt to add ehcache, not completed

--- trunk/core-bc/composites/svc/src/test/java/se/vgregion/medcontrol/services/MedcontrolDeviationServiceTest.java
+++ trunk/core-bc/composites/svc/src/test/java/se/vgregion/medcontrol/services/MedcontrolDeviationServiceTest.java
@@ -17,9 +17,7 @@
  */
 package se.vgregion.medcontrol.services;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 import java.io.StringWriter;
 import java.util.ArrayList;
@@ -35,109 +33,107 @@ import org.junit.Before;
 import org.junit.Test;
 
 import se.vgregion.medcontrol.domain.DeviationCase;
-import se.vgregion.portal.medcontrol.ws.ArrayOfCase;
 import se.vgregion.portal.medcontrol.ws.Case;
-import se.vgregion.portal.medcontrol.ws.MyCasesServiceSoap;
 
 public class MedcontrolDeviationServiceTest {
 
-  private static final String USER_1 = "user-1";
-  private MedcontrolDeviationService medcontrolDeviationService;
-  private MockMyCasesServiceSoap mockMyCasesServiceSoap;
-  private List<Case> cases;
+    private static final String USER_1 = "user-1";
+    private MedcontrolDeviationService medcontrolDeviationService;
+    private MockMyCasesServiceSoap mockMyCasesServiceSoap;
+    private List<Case> cases;
 
-  @Before
-  public void setUp() throws Exception {
-    medcontrolDeviationService = new MedcontrolDeviationService();
-    mockMyCasesServiceSoap = new MockMyCasesServiceSoap();
-    medcontrolDeviationService.setMyCasesServiceSoap(mockMyCasesServiceSoap);
-    generateCaseList();
-  }
+    @Before
+    public void setUp() throws Exception {
+        medcontrolDeviationService = new MedcontrolDeviationService();
+        mockMyCasesServiceSoap = new MockMyCasesServiceSoap();
+        medcontrolDeviationService.setMyCasesServiceSoap(mockMyCasesServiceSoap);
+        generateCaseList();
+    }
 
-  private void generateCaseList() {
-    cases = new ArrayList<Case>();
-    Case mockCase1 = new Case();
-    mockCase1.setCaseNo("mockCase1");
-    mockCase1.setDescription("desc1");
-    mockCase1.setHasActingRole(true);
-    mockCase1.setRegisteredDate("2009-12-09");
-    mockCase1.setUrl("http://test.com");
+    private void generateCaseList() {
+        cases = new ArrayList<Case>();
+        Case mockCase1 = new Case();
+        mockCase1.setCaseNo("mockCase1");
+        mockCase1.setDescription("desc1");
+        mockCase1.setHasActingRole(true);
+        mockCase1.setRegisteredDate("2009-12-09");
+        mockCase1.setUrl("http://test.com");
 
-    Case mockCase2 = new Case();
-    mockCase2.setCaseNo("mockCase2");
-    mockCase2.setDescription("desc2");
-    mockCase2.setHasActingRole(true);
-    mockCase2.setRegisteredDate("2009-12-10");
-    mockCase2.setUrl("http://test2.com");
+        Case mockCase2 = new Case();
+        mockCase2.setCaseNo("mockCase2");
+        mockCase2.setDescription("desc2");
+        mockCase2.setHasActingRole(true);
+        mockCase2.setRegisteredDate("2009-12-10");
+        mockCase2.setUrl("http://test2.com");
 
-    cases.add(mockCase1);
-    cases.add(mockCase2);
-    // Add mock case to list
-    mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(cases);
+        cases.add(mockCase1);
+        cases.add(mockCase2);
+        // Add mock case to list
+        mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(cases);
 
-  }
+    }
 
-  @Test
-  public void testListDeviationCases() {
-    List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
-    assertEquals(2, listDeviationCases.size());
-    // Assert case 1
-    assertEquals(cases.get(0).getCaseNo(), listDeviationCases.get(0).getCaseNumber());
-    assertEquals(cases.get(0).getDescription(), listDeviationCases.get(0).getDescription());
-    assertEquals(cases.get(0).isHasActingRole(), listDeviationCases.get(0).isHasActingRole());
-    assertNotNull(listDeviationCases.get(0).getRegisteredDate());
-    assertEquals(cases.get(0).getUrl(), listDeviationCases.get(0).getUrl());
+    @Test
+    public void testListDeviationCases() {
+        List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
+        assertEquals(2, listDeviationCases.size());
+        // Assert case 1
+        assertEquals(cases.get(0).getCaseNo(), listDeviationCases.get(0).getCaseNumber());
+        assertEquals(cases.get(0).getDescription(), listDeviationCases.get(0).getDescription());
+        assertEquals(cases.get(0).isHasActingRole(), listDeviationCases.get(0).isHasActingRole());
+        assertNotNull(listDeviationCases.get(0).getRegisteredDate());
+        assertEquals(cases.get(0).getUrl(), listDeviationCases.get(0).getUrl());
 
-    // Assert case 2
-    assertEquals(cases.get(1).getCaseNo(), listDeviationCases.get(1).getCaseNumber());
-    assertEquals(cases.get(1).getDescription(), listDeviationCases.get(1).getDescription());
-    assertEquals(cases.get(1).isHasActingRole(), listDeviationCases.get(1).isHasActingRole());
-    assertNotNull(listDeviationCases.get(1).getRegisteredDate());
-    assertEquals(cases.get(1).getUrl(), listDeviationCases.get(1).getUrl());
+        // Assert case 2
+        assertEquals(cases.get(1).getCaseNo(), listDeviationCases.get(1).getCaseNumber());
+        assertEquals(cases.get(1).getDescription(), listDeviationCases.get(1).getDescription());
+        assertEquals(cases.get(1).isHasActingRole(), listDeviationCases.get(1).isHasActingRole());
+        assertNotNull(listDeviationCases.get(1).getRegisteredDate());
+        assertEquals(cases.get(1).getUrl(), listDeviationCases.get(1).getUrl());
 
-  }
+    }
 
-  @Test
-  public void testNullUser() {
-    List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(null);
-    assertEquals(0, listDeviationCases.size());
-  }
+    @Test
+    public void testNullUser() {
+        List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(null);
+        assertEquals(0, listDeviationCases.size());
+    }
 
-  @Test
-  public void testNullDateValue() {
-    mockMyCasesServiceSoap.getMockArrayOfCase().getCase().clear();
-    mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(Arrays.asList(new Case()));
-    List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
-    assertEquals(1, listDeviationCases.size());
-  }
+    @Test
+    public void testNullDateValue() {
+        mockMyCasesServiceSoap.getMockArrayOfCase().getCase().clear();
+        mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(Arrays.asList(new Case()));
+        List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
+        assertEquals(1, listDeviationCases.size());
+    }
 
-  @Test
-  public void testInvalidDateFormat() {
- 
-    StringWriter loggerView = getLoggerView(Level.ERROR);
-    Case case1 = new Case();
-    case1.setRegisteredDate("12-12-2009");
-    mockMyCasesServiceSoap.getMockArrayOfCase().getCase().clear();
-    mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(Arrays.asList(case1));
-    List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
-    assertEquals(1, listDeviationCases.size());
-    assertTrue(loggerView.toString().contains("Not valid date"));
-  }
-  
-  @Test(expected = DeviationServiceException.class)
-  public void testWebServiceException() {
-    mockMyCasesServiceSoap.setThrowException(true);
-    StringWriter loggerView = getLoggerView(Level.ERROR);
-    medcontrolDeviationService.listDeviationCases(USER_1);
-    assertTrue(loggerView.toString().contains("MedControl webservice exception"));
-  }
-  
-  private StringWriter getLoggerView(Level logLevel) {
-    Logger logger = Logger.getLogger(MedcontrolDeviationService.class);
-    logger.setLevel(logLevel);
-    final StringWriter writer = new StringWriter();
-    Appender appender = new WriterAppender(new SimpleLayout(), writer);
-    logger.addAppender(appender);
-    return writer;
-  }
+    @Test
+    public void testInvalidDateFormat() {
+
+        StringWriter loggerView = getLoggerView(Level.ERROR);
+        Case case1 = new Case();
+        case1.setRegisteredDate("12-12-2009");
+        mockMyCasesServiceSoap.getMockArrayOfCase().getCase().clear();
+        mockMyCasesServiceSoap.getMockArrayOfCase().getCase().addAll(Arrays.asList(case1));
+        List<DeviationCase> listDeviationCases = medcontrolDeviationService.listDeviationCases(USER_1);
+        assertEquals(1, listDeviationCases.size());
+        assertTrue(loggerView.toString().contains("Not valid date"));
+    }
+
+    @Test(expected = DeviationServiceException.class)
+    public void testWebServiceException() {
+        mockMyCasesServiceSoap.setThrowException(true);
+        StringWriter loggerView = getLoggerView(Level.ERROR);
+        medcontrolDeviationService.listDeviationCases(USER_1);
+        assertTrue(loggerView.toString().contains("MedControl webservice exception"));
+    }
+
+    private StringWriter getLoggerView(Level logLevel) {
+        Logger logger = Logger.getLogger(MedcontrolDeviationService.class);
+        logger.setLevel(logLevel);
+        final StringWriter writer = new StringWriter();
+        Appender appender = new WriterAppender(new SimpleLayout(), writer);
+        logger.addAppender(appender);
+        return writer;
+    }
 }
