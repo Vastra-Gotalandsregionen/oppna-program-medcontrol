cbf87fdcf6d63b4906edaa03a2fcd3d6cccaba44
wmdaros <wmdaros@e58ce578-e49c-11de-b136-e12340cf38ae>
2011-03-16 09:43:17 +0000

    [maven-release-plugin]  copy for tag medcontrol-system-1.6

---
+++ tags/medcontrol-system-1.6/core-bc/composites/svc/src/main/java/se/vgregion/medcontrol/services/DeviationServiceException.java
@@ -0,0 +1,54 @@
+/**
+ * Copyright 2010 Västra Götalandsregionen
+ *
+ *   This library is free software; you can redistribute it and/or modify
+ *   it under the terms of version 2.1 of the GNU Lesser General Public
+ *   License as published by the Free Software Foundation.
+ *
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public
+ *   License along with this library; if not, write to the
+ *   Free Software Foundation, Inc., 59 Temple Place, Suite 330,
+ *   Boston, MA 02111-1307  USA
+ *
+ */
+
+package se.vgregion.medcontrol.services;
+
+/**
+ * Exception class to show runtime exceptions from DeviationService.
+ * 
+ * @author David Bennehult
+ * @author Anders Bergkvist
+ */
+public class DeviationServiceException extends RuntimeException {
+
+    private static final long serialVersionUID = 5737322772912614407L;
+
+    /**
+     * Constructor that accepts the exception message string.
+     * 
+     * @param message
+     *            String message, explaining the error.
+     */
+    public DeviationServiceException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructor that accepts the exception message string and the exception object.
+     * 
+     * @param message
+     *            String message, explaining the error.
+     * @param e
+     *            The error object
+     */
+    public DeviationServiceException(String message, Throwable e) {
+        super(message, e);
+    }
+
+}
